85e62087c7a2326f6366186435b183a9c00aadb5
Jesse Otim <otimdanieljesse@gmail.com>
2023-12-15 12:44:15 +0300

    Refactor code

--- server.js
+++ server.js
@@ -23,25 +23,46 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
-//Home route
+// //Home route
+// app.get('/', (req, res) => {
+//     return res.status(200).send({
+//       status: 200,
+//       message: 'warming up at Port 5000'
+//     });
+// });
+
+// // wrong route
+// app.use((req, res) => res.status(405).send({
+//     'status': 405,
+//     'error': 'This URL does not exist'
+// }));
+
+// // server down
+// app.use((req, res) => res.status(500).send({
+//     'status': 500,
+//     'error': 'Oops! The problem is not on your side. Hang on, we will fix this soon'
+// }));
+
+
+// Error handling middleware for invalid routes
+app.use((req, res) => res.status(405).send({
+    'status': 405,
+    'error': 'This URL does not exist'
+}));
+
+// Error handling middleware for server errors
+app.use((req, res) => res.status(500).send({
+    'status': 500,
+    'error': 'Oops! The problem is not on your side. Hang on, we will fix this soon'
+}));
+
+// Home route
 app.get('/', (req, res) => {
     return res.status(200).send({
       status: 200,
       message: 'warming up at Port 5000'
     });
 });
-
-// wrong route
-app.use((req, res) => res.status(405).send({
-    'status': 405,
-    'error': 'This URL does not exist'
-}));
-
-// server down
-app.use((req, res) => res.status(500).send({
-    'status': 500,
-    'error': 'Oops! The problem is not on your side. Hang on, we will fix this soon'
-}));
 
 // current process environment
 app.listen(PORT || 3000,
